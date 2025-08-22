2f8babb39c1f606ada4e70e0611a268575b6f7c3
DebHtm <yolm9yolm@gmail.com>
2025-08-23 02:58:41 +0300

    lastDOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,33 +10,36 @@ import people from './people.json';
     const row = document.createElement('tr');
 
     const tdName = document.createElement('td');
+
     tdName.textContent = person.name;
     row.appendChild(tdName);
 
     const tdGender = document.createElement('td');
+
     tdGender.textContent =
-      person.sex === 'm' ? 'Male' :
-      person.sex === 'f' ? 'Female' : '';
+      person.sex === 'm' ? 'Male' : person.sex === 'f' ? 'Female' : '';
     row.appendChild(tdGender);
 
     const tdBorn = document.createElement('td');
+
     tdBorn.textContent = person.born;
     row.appendChild(tdBorn);
 
     const tdDied = document.createElement('td');
+
     tdDied.textContent = person.died ?? '';
     row.appendChild(tdDied);
 
     const tdAge = document.createElement('td');
+
     tdAge.textContent = person.died
       ? person.died - person.born
       : currentYear - person.born;
     row.appendChild(tdAge);
 
     const tdCentury = document.createElement('td');
-    tdCentury.textContent = person.died
-      ? Math.ceil(person.died / 100)
-      : '';
+
+    tdCentury.textContent = person.died ? Math.ceil(person.died / 100) : '';
     row.appendChild(tdCentury);
 
     dashboardTable.appendChild(row);
